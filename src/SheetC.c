128c9226ee0bfa93aedcfa0d6a5cf8186d48ce04
Viniguipc <138828870+Viniguipc@users.noreply.github.com>
2026-03-22 16:51:14 -0300

    Adicionando Vida e Mana
    Foi adicionado uma struct de personagem com vida e mana e as funções para editar a vida e a mana do personagem

--- src/SheetC.c
+++ src/SheetC.c
@@ -1,5 +1,59 @@
 #include <stdio.h>
 
-main(){
-	printf("Hello, SheetC");
+typedef struct pj{
+    char nome[40];
+    int mana;
+    int vida;
+}pp;
+
+void menu (int* op){
+    do{
+        printf("\tMENU\n");
+        printf("\n---------");
+        printf("\n1 - Atualizar Vida\n2 - Atualizar Mana\n3 - Sair");
+        printf("\n---------\n");
+        scanf("%d", op);
+    }while (*op < 1 || *op > 3);
+}
+
+void atualizar_vida(pp* pj){
+    int op;
+    
+    do{
+        printf("\n1 - Aumentar\n2 - Diminuir\n");
+        scanf(" %d", &op);
+    }while (op < 1 || op > 2);
+    
+    switch(op){
+        case 1:
+            break;
+        case 2:
+            break;    
+    }
+}
+
+void atualizar_mana(){
+    
+}
+
+int main(){
+    pp pj;
+    int i, op;
+    
+    pj.vida = 50;
+    pj.mana = 45;
+    
+    do{
+        menu(&op);
+        
+        switch(op){
+            case 1:
+                atualizar_vida(&pj);
+                break;
+            case 2:
+                atualizar_mana();
+                break;
+        }
+    }while (op != 3);
+    
 }
