173ff3b9d2381621a7b729f97e924250f2baf43e
Viniguipc <138828870+Viniguipc@users.noreply.github.com>
2026-03-22 17:16:32 -0300

    Impressao e funcoes vida e mana
    Adicionando função de impressão no terminal dos dados do personagem e terminado funções para editar vida e mana

--- src/SheetC.c
+++ src/SheetC.c
@@ -1,10 +1,18 @@
 #include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
 
 typedef struct pj{
     char nome[40];
     int mana;
     int vida;
 }pp;
+
+void imprimir_personagem (pp pj){
+    printf("\nPersonagem atualizado: \n");
+    printf("\nNome: %s", pj.nome);
+    printf("\nVida: %d | Mana: %d\n", pj.vida, pj.mana);
+}
 
 void menu (int* op){
     do{
@@ -17,7 +25,7 @@ void menu (int* op){
 }
 
 void atualizar_vida(pp* pj){
-    int op;
+    int op, vida;
     
     do{
         printf("\n1 - Aumentar\n2 - Diminuir\n");
@@ -26,24 +34,64 @@ void atualizar_vida(pp* pj){
     
     switch(op){
         case 1:
+            do{
+                printf("\nDigite a vida recuperada: ");
+                scanf(" %d", &vida);
+            }while (vida < 0);
+            
+            pj->vida = pj->vida + vida;
             break;
         case 2:
+            do{
+                printf("\nDigite a vida perdida: ");
+                scanf(" %d", &vida);
+                
+                pj->vida = pj->vida - vida;
+            }while (vida < 0);
             break;    
     }
 }
 
-void atualizar_mana(){
+void atualizar_mana(pp* pj){
+    int op, mana;
     
+    do{
+        printf("\n1 - Aumentar\n2 - Diminuir\n");
+        scanf(" %d", &op);
+    }while (op < 1 || op > 2);
+    
+    switch(op){
+        case 1:
+            do{
+                printf("\nDigite a mana recuperada: ");
+                scanf(" %d", &mana);
+            }while (mana < 0);
+            
+            pj->mana = pj->mana + mana;
+            break;
+        case 2:
+            do{
+                printf("\nDigite a vida perdida: ");
+                scanf(" %d", &mana);
+                
+                pj->mana = pj->mana - mana;
+            }while (mana < 0);
+            break;    
+    }
 }
 
 int main(){
     pp pj;
     int i, op;
     
+    strcpy(pj.nome, "Drugo"); 
     pj.vida = 50;
     pj.mana = 45;
     
     do{
+        system("clear");
+        imprimir_personagem(pj);
+        
         menu(&op);
         
         switch(op){
@@ -51,9 +99,8 @@ int main(){
                 atualizar_vida(&pj);
                 break;
             case 2:
-                atualizar_mana();
+                atualizar_mana(&pj);
                 break;
         }
     }while (op != 3);
-    
 }
