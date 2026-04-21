01f73d6ecb837bdf7861521e4c5242b0edd7edf4
Viniguipc <viniguipc@gmail.com>
2026-04-21 00:19:03 -0300

    menu load

--- src/menu.c
+++ src/menu.c
@@ -9,6 +9,62 @@ void menu_incial(int *estado){
     //Se pressionar Enter
     if(IsKeyPressed(KEY_ENTER)){
         *estado = opcao_atual;
+        opcao_atual = 1;
+    }
+
+    //Movimentação entre as opções
+    if (IsKeyPressed(KEY_DOWN)){
+        if(opcao_atual == num_opcoes){
+            opcao_atual = 1;
+        }
+        else{
+            opcao_atual++;
+        }
+    }
+    if (IsKeyPressed(KEY_UP)){
+        if(opcao_atual == 1){
+            opcao_atual = num_opcoes;
+        }
+        else{
+            opcao_atual--;
+        }
+    }
+
+    //Draw
+	BeginDrawing();
+		ClearBackground(RAYWHITE);
+        DrawText("RPG Sheet C", (GetScreenWidth() - MeasureText("RPG Sheet C", GetScreenHeight() * 0.10)) / 2, (GetScreenHeight() * 0.20), GetScreenHeight() * 0.10, BLACK);
+
+        for(int i = 0; i < num_opcoes; i++){
+			if(i + 1 != opcao_atual){
+				DrawText(opcoes[i], (GetScreenWidth() - MeasureText(opcoes[i], GetScreenHeight() * 0.05)) / 2, (GetScreenHeight() * 0.50) + (i * (GetScreenHeight() * 0.05)), GetScreenHeight() * 0.05, BLACK);
+			}
+            else{
+                DrawText(opcoes[i], (GetScreenWidth() - MeasureText(opcoes[i], GetScreenHeight() * 0.05)) / 2, (GetScreenHeight() * 0.50) + (i * (GetScreenHeight() * 0.05)), GetScreenHeight() * 0.05, RED);
+            }
+        }
+	EndDrawing();
+}
+
+void menu_load(int *estado){
+	static int opcao_atual = 1;
+	char *opcoes[] = {"Criar Ficha", "Carregar Ficha", "Voltar"};
+	int num_opcoes = 3;
+
+    //Se pressionar Enter
+    if(IsKeyPressed(KEY_ENTER)){
+        switch(opcao_atual){
+            case 1:
+                *estado = 4;
+                break;
+            case 2:
+                *estado = 5;
+                break;
+            case 3:
+                *estado = 0;
+                break;
+        }
+        opcao_atual = 1;
     }
 
     //Movimentação entre as opções
--- src/menu.h
+++ src/menu.h
@@ -2,5 +2,6 @@
 #define MENU_H
 
 void menu_incial(int *estado);
+void menu_load(int *estado);
 
 #endif
